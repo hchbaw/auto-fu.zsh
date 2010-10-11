64cf011d7d14ca62d9e10e71fce745ae57d216c4
Takeshi Banse <takebi@laafc.net>
2010-10-11 15:11:21 +0900

    One more warnig fix.
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- auto-fu.zsh
+++ auto-fu.zsh
@@ -248,6 +248,7 @@ declare -a afu_accept_lines
 afu-recursive-edit-and-accept () {
   local -a __accepted
   zle recursive-edit -K afu || { afu-reset; zle -R ''; zle send-break; return }
+  [[ -n ${__accepted} ]] &&
   (( ${#${(M)afu_accept_lines:#${__accepted[1]}}} > 1 )) &&
   { zle "${__accepted[@]}"} || { zle accept-line }
 }
